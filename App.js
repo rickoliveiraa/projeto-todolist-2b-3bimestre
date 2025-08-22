1fcb0e1f642700e704864544b3abb6bc9ac7ac0c
Ana Clara Oliveira Bezerra <anaclara00938@gmail.com>
2025-08-22 11:06:07 -0300

    [feat] Input de Tarefa

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Alert, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import logo from './assets/pngegg.png'
+import btnAdd from './assets/btnAdd.png'
+import { useState } from 'react';
 
 export default function App() {
-  return (
+  const [tarefa, setTarefa] = useState("")
+
+  const btnAdicionar = () =>{
+     Alert.alert("TODO List", "Valor:" + tarefa);
+  setTarefa("")
+  };
+ return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.viewLogo}>
         <Image source={logo} style={styles.logo}/>
+         <Text>TODO List</Text>
       </View>
-      <Text>Projeto 2B - 12+1/08/2025</Text>
+      <View style={styles.viewInput}>
+        <TextInput placeholder='Digite a tarefa'
+        value={tarefa}
+        onChangeText={setTarefa}/> <TouchableOpacity onPress={btnAdicionar}>
+        <Image source={btnAdd} style={style.btnAdd}/>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,9 +35,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
   logo: {
     height:128,
     width:128,
+  },
+  viewInput: {
+    width: "100%",
+    justifyContent: "space-between",
+    flexDirection: "row" 
+  },
+  btnAdd: {
+    width:32,
+    height:32
+  },
+  viewLogo: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10
+
   }
 });
